ce1374b88066837ee1107fda1c42e65da9dd1ec3
bharathjinka09 <bharathjinka09@gmail.com>
2020-05-05 16:55:54 +0530

    Images uploaded successfully to folder and db with unique id

--- insert.php
+++ insert.php
@@ -7,41 +7,71 @@
 		  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
 		  exit();
 		}
+					
+			$name = $_POST['name'];
+			$mobile = $_POST['mobile'];
+			$whatsapp = $_POST['whatsapp'];
+			$email = $_POST['email'];
+			$address = $_POST['address'];
+			$gstin = $_POST['gstin'];
+			$service = $_POST['service'];
+			$service_area = $_POST['service_area'];
+			$bank_name = $_POST['bank_name'];
+			$beneficiary_name = $_POST['beneficiary_name'];
+			$account_number = $_POST['account_number'];
+			$bank_address = $_POST['bank_address'];
+			$ifsc = $_POST['ifsc'];
+			$payment_mode = $_POST['payment_mode'];
+			
+			if (isset($_FILES["id_proof"]["name"])) {
 
-		
-		$name = $_POST['name'];
-		$mobile = $_POST['mobile'];
-		$whatsapp = $_POST['whatsapp'];
-		$email = $_POST['email'];
-		$address = $_POST['address'];
-		$gstin = $_POST['gstin'];
-		$service = $_POST['service'];
-		$service_area = $_POST['service_area'];
-		$bank_name = $_POST['bank_name'];
-		$beneficiary_name = $_POST['beneficiary_name'];
-		$account_number = $_POST['account_number'];
-		$bank_address = $_POST['bank_address'];
-		$ifsc = $_POST['ifsc'];
-		$payment_mode = $_POST['payment_mode'];
-		$id_proof = $_POST['id_proof'];
-		// $driving_license = $_POST['driving_license'];
-		
-		// echo $name;
-		// echo $mobile;
-		// echo $whatsapp;
+			    $id_proof = $_FILES["id_proof"]["name"];
+			    $tmp_name = $_FILES['id_proof']['tmp_name'];
+			    $error = $_FILES['id_proof']['error'];
 
-		// Perform query
+			    if (!empty($name)) {
+			        $location = 'images/';
 
-		$mysqli -> query(
-			"INSERT INTO members (name,mobile,whatsapp,email,address,
-			gstin,service,service_area,bank_name,beneficiary_name,account_number,
-			bank_address,ifsc,payment_mode,id_proof,driving_license) VALUES('$name','$mobile','$whatsapp'
-			,'$email','$address','$gstin','$service','$service_area','$bank_name','$beneficiary_name','$account_number','$bank_address','$ifsc','$payment_mode','','')"
-		);
+			        if  (move_uploaded_file($tmp_name, $location.rand(10,1000).$id_proof)){
+			            echo '';
+			        }
 
-		$mysqli -> close();
+			    } else {
+			        echo 'please choose a file';
+			    }
+			}
 
-		echo "<h2 style='color:green; text-align:center;'><i>Thank you $name. You have registered successfully!</i></h2>";
-		header("Refresh:3; url=index.html");
+			if (isset($_FILES["driving_license"]["name"])) {
+
+			    $driving_license = $_FILES["driving_license"]["name"];
+			    // echo $driving_license;
+			    $tmp_name = $_FILES['driving_license']['tmp_name'];
+			    $error = $_FILES['driving_license']['error'];
+
+			    if (!empty($name)) {
+			        $location = 'images/';
+
+			        if  (move_uploaded_file($tmp_name, $location.$driving_license)){
+			            echo 'Uploaded';
+			        }
+
+			    } else {
+			        echo 'please choose a file';
+			    }
+			}
+
+			// Perform query
+
+			$mysqli -> query(
+				"INSERT INTO members (name,mobile,whatsapp,email,address,
+				gstin,service,service_area,bank_name,beneficiary_name,account_number,
+				bank_address,ifsc,payment_mode,id_proof,driving_license) VALUES('$name','$mobile','$whatsapp'
+				,'$email','$address','$gstin','$service','$service_area','$bank_name','$beneficiary_name','$account_number','$bank_address','$ifsc','$payment_mode','$id_proof','$driving_license')"
+			);
+
+			$mysqli -> close();
+
+			echo "<h2 style='color:green; text-align:center;'><i>Thank you $name. You have registered successfully!</i></h2>";
+			// header("Refresh:3; url=index.html");
 
 ?>
