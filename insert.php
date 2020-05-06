ad9afec1396e44b60a04693792b1f3e64f7ffd9d
bharathjinka09 <bharathjinka09@gmail.com>
2020-05-06 10:49:45 +0530

    Added multiple service selection option

--- insert.php
+++ insert.php
@@ -14,7 +14,7 @@
 			$email = $_POST['email'];
 			$address = $_POST['address'];
 			$gstin = $_POST['gstin'];
-			$service = $_POST['service'];
+			$services = $_POST['services'];
 			$service_area = $_POST['service_area'];
 			$bank_name = $_POST['bank_name'];
 			$beneficiary_name = $_POST['beneficiary_name'];
@@ -64,14 +64,27 @@
 			$id = uniqid();
 
 			// KABLR40001
-
+			
+			foreach($services as $service){
+    			// $mysqli -> query("insert into members values('','$service')");
+				// $a=array("red","green");
+				// array_push($a,"blue","yellow");
+				// print_r($a);
+				// $services_list = array();
+				// array_push($services_list, $service);
+				// print_r($services_list);
+			
 			$mysqli -> query(
 				"INSERT INTO members (name,mobile,whatsapp,email,address,
 				gstin,service,service_area,bank_name,beneficiary_name,account_number,
-				bank_address,ifsc,payment_mode,id_proof,driving_license,vendor_id) VALUES('$name','$mobile','$whatsapp'
-				,'$email','$address','$gstin','$service','$service_area','$bank_name','$beneficiary_name','$account_number','$bank_address','$ifsc','$payment_mode','$location$email$id_proof','$location$email$driving_license','KABLR4$id')"
-			);
+				bank_address,ifsc,payment_mode,id_proof,driving_license,vendor_id) 
 
+				VALUES('$name','$mobile','$whatsapp','$email','$address','$gstin',
+				'$service','$service_area','$bank_name','$beneficiary_name',
+				'$account_number','$bank_address','$ifsc','$payment_mode',
+				'$location$email$id_proof','$location$email$driving_license','KABLR4$id')"
+			);
+		}
 			$mysqli -> close();
 
 			echo "<h2 style='color:green; text-align:center;'><i>Thank you $name. You have registered successfully!</i></h2>";
